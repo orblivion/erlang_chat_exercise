cb147f770e6554c822ab832da0c952b9cb14df26
Daniel Krol <orblivion@gmail.com>
2014-05-04 16:54:52 -0700

    Think I have sending in the server, want to save

--- chat_server.erl
+++ chat_server.erl
@@ -14,12 +14,19 @@ new_connections(ConnectionManager, LSock) ->
     new_connections(ConnectionManager, LSock).
 
 handle_connection(ConnectionManager, Sock, Bs) ->
-    case gen_tcp:recv(Sock, 0) of
-        {ok, B} ->
-            handle_connection(ConnectionManager, Sock, [Bs, B]);
-        {error, closed} ->
-            ConnectionManager ! {remove_sock, Sock},
-            {ok, list_to_binary(Bs)}
+    case binary:split(Bs, <<"\n">>) of
+        [Msg|Next] ->
+            io:format(">~p", [Msg]),
+            ConnectionManager ! {send_msg, self(), Sock, Msg},
+            handle_connection(ConnectionManager, Sock, Next);
+        _ ->
+            case gen_tcp:recv(Sock, 0) of
+                {ok, B} ->
+                    handle_connection(ConnectionManager, Sock, [Bs, B]);
+                {error, closed} ->
+                    ConnectionManager ! {remove_sock, Sock},
+                    {ok, list_to_binary(Bs)}
+            end
     end.
 
 manage_connections(Socks) ->
@@ -29,7 +36,11 @@ manage_connections(Socks) ->
             spawn(fun() -> handle_connection(ConnectioonManager, Sock, []) end),
             manage_connections([Sock|Socks]);
         {remove_sock, Sock} -> 
-            manage_connections(lists:delete(Sock, Socks))
+            manage_connections(lists:delete(Sock, Socks));
+        {send_msg, Sender, SenderSock, Msg} -> 
+            FormattedMsg = lists:flatten(io_lib:format("~p: ~p", [Sender, Msg])),
+            Recipients = lists:delete(SenderSock, Socks),
+            lists:map(fun(Sock) -> gen_tcp:send(Sock, FormattedMsg) end, Recipients)
     end.
 
 %spit(LSock) ->
