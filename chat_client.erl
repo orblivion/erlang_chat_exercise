c5a37499de0c0d15e8665114804c0b997ebcc449
Daniel Krol <orblivion@gmail.com>
2014-05-05 22:16:11 -0700

    Chat client works, needs a couple touchups

--- chat_client.erl
+++ chat_client.erl
@@ -1,9 +1,43 @@
 -module(chat_client).
--export([client/1]).
+-export([client/0]).
 
-client(Text) ->
+client() ->
     SomeHostInNet = "localhost", % to make it runnable on one machine
     {ok, Sock} = gen_tcp:connect(SomeHostInNet, 5678, 
-                                 [binary, {packet, 0}]),
-    ok = gen_tcp:send(Sock, Text),
+                                 [binary, {packet, 0}, {active, false}]),
+    spawn(fun() -> recv_loop(Sock, []) end),
+    send_loop(Sock),
     ok = gen_tcp:close(Sock).
+
+send_loop(Sock) ->
+    Msg = io:get_line("Me >"),
+    case Msg of
+        "/end\n" -> ok;
+        _ -> gen_tcp:send(Sock, Msg), send_loop(Sock)
+    end.
+
+recv_loop(Sock, Accum) ->
+    case gen_tcp:recv(Sock, 0) of
+        {ok, B} ->
+            case binary:split(B, <<"\n">>) of
+                [MsgEnd|Next] ->
+                    io:format([Accum, MsgEnd]),
+                    io:format("\n"),
+                    recv_loop(Sock, [Next]);
+                _ ->
+                    recv_loop(Sock, [Accum, B])
+            end;
+        {error, closed} ->
+            ok
+    end.
+
+
+
+
+
+
+
+
+
+
+
--- chat_server.erl
+++ chat_server.erl
@@ -6,27 +6,31 @@ server() ->
                                         {active, false}]),
     ConnectioonManager = self(),
     spawn(fun() -> new_connections(ConnectioonManager, LSock) end),
-    manage_connections([]).
+    manage_connections([]),
+    io:format("Ending. At least I should be.").
 
 new_connections(ConnectionManager, LSock) ->
     {ok, Sock} = gen_tcp:accept(LSock),
     ConnectionManager ! {add_sock, Sock},
     new_connections(ConnectionManager, LSock).
 
-handle_connection(ConnectionManager, Sock, Bs) ->
-    case binary:split(Bs, <<"\n">>) of
-        [Msg|Next] ->
-            io:format(">~p", [Msg]),
-            ConnectionManager ! {send_msg, self(), Sock, Msg},
-            handle_connection(ConnectionManager, Sock, Next);
-        _ ->
-            case gen_tcp:recv(Sock, 0) of
-                {ok, B} ->
-                    handle_connection(ConnectionManager, Sock, [Bs, B]);
-                {error, closed} ->
-                    ConnectionManager ! {remove_sock, Sock},
-                    {ok, list_to_binary(Bs)}
-            end
+handle_connection(ConnectionManager, MySock, Accum) ->
+    case gen_tcp:recv(MySock, 0) of
+        {ok, B} ->
+            case binary:split(B, <<"\n">>) of
+                [MsgEnd|Next] ->
+                    Msg = binary_to_list(list_to_binary([Accum, MsgEnd])),
+                    case Msg of
+                        "kill" -> ConnectionManager ! die;
+                        _ ->
+                            ConnectionManager ! {send_msg, self(), MySock, Msg},
+                            handle_connection(ConnectionManager, MySock, [Next])
+                    end;
+                _ ->
+                    handle_connection(ConnectionManager, MySock, [Accum, B])
+            end;
+        {error, closed} ->
+            ConnectionManager ! {remove_sock, MySock}
     end.
 
 manage_connections(Socks) ->
@@ -38,21 +42,9 @@ manage_connections(Socks) ->
         {remove_sock, Sock} -> 
             manage_connections(lists:delete(Sock, Socks));
         {send_msg, Sender, SenderSock, Msg} -> 
-            FormattedMsg = lists:flatten(io_lib:format("~p: ~p", [Sender, Msg])),
+            FormattedMsg = io_lib:format("~p: ~p", [Sender, Msg]),
             Recipients = lists:delete(SenderSock, Socks),
-            lists:map(fun(Sock) -> gen_tcp:send(Sock, FormattedMsg) end, Recipients)
+            lists:map(fun(Sock) -> gen_tcp:send(Sock, FormattedMsg) end, Recipients),
+            manage_connections(Socks);
+        {die} -> ok
     end.
-
-%spit(LSock) ->
-%    {ok, Sock} = gen_tcp:accept(LSock),
-%    {ok, Bin} = do_recv(Sock, []),
-%    case Bin of
-%        <<"exit">> ->
-%            ok;
-%        _ ->
-%            ok = gen_tcp:close(Sock),
-%            io:format(Bin),
-%            spit(LSock)
-%    end.
-%
-
